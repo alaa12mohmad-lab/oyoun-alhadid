1ed9f79f1fe8cc871421e624949b590531559a37
alaa12mohmad-lab <alaa12mohmad@gmail.com>
2026-08-15 11:35:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/oyoun-alhadid/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-v2.js`,
+        chunkFileNames: `assets/[name]-[hash]-v2.js`,
+      }
+    }
+  }
 })
