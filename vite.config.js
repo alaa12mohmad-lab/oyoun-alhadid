4acb675bc0a09750c88bec83b58cd13155b301dc
alaa12mohmad-lab <alaa12mohmad@gmail.com>
2026-09-05 13:36:11 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name]-[hash]-v2.js`,
-        chunkFileNames: `assets/[name]-[hash]-v2.js`,
+        entryFileNames: `assets/[name]-[hash]-v3.js`,
+        chunkFileNames: `assets/[name]-[hash]-v3.js`,
       }
     }
   }
